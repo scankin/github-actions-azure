5e9c25979b01363dd2ac3dbb42acb7e3ec85f25e
SCANKIN <scott.ankin@devopsgroup.com>
2022-02-04 14:48:02 +0000

    Bug fix

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.31.1"
+      version = "~>2.0"
     }
   }
 
@@ -15,6 +15,8 @@ terraform {
 }
 
 provider "azurerm" {
+  version = "~>2.0"
+
   features {}
 }
 
