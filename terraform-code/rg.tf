18976e1933ac6a76e8158480d419bfad58df4232
SCANKIN <scott.ankin@devopsgroup.com>
2022-02-04 14:52:14 +0000

    Bug fix

--- terraform-code/rg.tf
+++ terraform-code/rg.tf
@@ -1,4 +1,4 @@
-resource azurerm_resource_group "rg" {
+resource "azurerm_resource_group" "rg" {
     name = join("-", ["actions", "sample", "rg", "uks"])
     location = "uksouth"
 }
--- terraform-code/vnet.tf
+++ terraform-code/vnet.tf
@@ -1,13 +1,14 @@
-resource azurerm_virtual_network "vnet" {
+resource "azurerm_virtual_network" "vnet" {
     name = join("-", ["github", "vnet", "actions"])
     location = "uksouth"
     address = ["192.168.0.0/16"]
     resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource azurerm_subnet "snet" {
+resource "azurerm_subnet" "snet" {
     name = join("-", ["github", "actions", "subnet"])
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet
     address_prefix = "192.168.0.0/24"
 }
+
