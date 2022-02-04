6a7b3823bbb680ece0232d4f4863b024c3384813
SCANKIN <scott.ankin@devopsgroup.com>
2022-02-04 15:03:20 +0000

    Bug fix

--- terraform-code/vnet.tf
+++ terraform-code/vnet.tf
@@ -9,6 +9,6 @@ resource "azurerm_subnet" "snet" {
     name = join("-", ["github", "actions", "subnet"])
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = "192.168.0.0/24"
+    address_prefixes = ["192.168.0.0/24"]
 }
 
